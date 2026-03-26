eb2cb6ee1ad2544680b91a84cb99a5cbed64d257
xiyori <foma@shipilov.ru>
2026-03-26 15:44:56 +0300

    Add footer

---
+++ global-top.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer
+    v-if="$nav.currentLayout !== 'cover'"
+    class="absolute bottom-4 left-0 right-4 text-right"
+  >
+    <br/><small><SlideCurrentNo/>/<SlidesTotal/></small><br/>
+  </footer>
+</template>
